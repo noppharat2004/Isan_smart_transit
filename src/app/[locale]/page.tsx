c65f72414098cf1ef321f62e94fa490b83b7d79e
noppharat2004 <noppharat5588@gmail.com>
2026-06-04 12:12:02 +0700

    Increase card spacing to space-y-8

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -199,7 +199,7 @@ export default function Home() {
         </Tabs>
 
         {/* Feed */}
-        <div className="space-y-6">
+        <div className="space-y-8">
           {filteredUpdates.length === 0 ? (
             <Card>
               <CardContent className="py-8 text-center text-gray-500">
